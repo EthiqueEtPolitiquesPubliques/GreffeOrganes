aa4ffcedc84d521d2b3bba58440c4987b5bbeeca
EthiqueEtPolitiquesPubliques <34211211+EthiqueEtPolitiquesPubliques@users.noreply.github.com>
2017-12-07 20:28:43 +0100

    Test PP

--- responses.js
+++ responses.js
@@ -1,13 +1,13 @@
 var responses = {
 
     "1":{
-        responsePositive:"Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia, tandem id ut optimum factu elegit: et Vrsicinum primum ad se venire summo cum honore mandavit ea specie ut pro rerum tunc urgentium captu disponeretur concordi consilio, quibus virium incrementis Parthicarum gentium a arma minantium impetus frangerentur.",
-        responseNegative:"Vous n'avez pas répondu OUI à la question 1, c'est pas bien !",
+        responsePositive:"Le prélèvement et la greffe d’organes sont autorisés dans le respect du cadre détaillé ci-dessous.",
+        responseNegative:"Le prélèvement et la greffe d’organes sont illégaux.",
         section:"1"
     },
     "2":{
-        responsePositive:"Bravo, vous êtes d'accord avec nous  sur la 2 !",
-        responseNegative:"Utque proeliorum periti rectores primo catervas densas opponunt et fortes, deinde leves armaturas, post iaculatores ultimasque subsidiales acies, si fors adegerit, iuvaturas, ita praepositis urbanae familiae suspensae digerentibus sollicite, quos insignes faciunt virgae dexteris aptatae velut tessera data castrensi iuxta vehiculi frontem omne textrinum incedit: huic atratum coquinae iungitur ministerium, dein totum promiscue servitium cum otiosis plebeiis de vicinitate coniunctis: postrema multitudo spadonum a senibus in pueros desinens, obluridi distortaque lineamentorum conpage deformes, ut quaqua incesserit quisquam cernens mutilorum hominum agmina detestetur memoriam Samiramidis reginae illius veteris, quae teneros mares castravit omnium prima velut vim iniectans naturae, eandemque ab instituto cursu retorquens, quae inter ipsa oriundi crepundia per primigenios seminis fontes tacita quodam modo lege vias propagandae posteritatis ostendit.",
+        responsePositive:"Il est autorisé de prélever les organes sur des personnes dont la mort a été dûment constatée.",
+        responseNegative:"Il est interdit de prélever les organes sur des personnes dont la mort a été dûment constatée.",
         section:"1"
     },
     "3":{
