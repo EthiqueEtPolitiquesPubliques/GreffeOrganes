f1cafb8cef5d6be7205896a39998f945e757d2d6
EthiqueEtPolitiquesPubliques <34211211+EthiqueEtPolitiquesPubliques@users.noreply.github.com>
2017-12-07 20:58:50 +0100

    Test section

--- responses.js
+++ responses.js
@@ -8,21 +8,48 @@ var responses = {
     "2":{
         responsePositive:"Il est autorisé de prélever les organes sur des personnes dont la mort a été dûment constatée.",
         responseNegative:"Il est interdit de prélever les organes sur des personnes dont la mort a été dûment constatée.",
-        section:"1"
+        section:"2"
     },
     "3":{
-        responsePositive:"Bravo, vous êtes d'accord avec nous  sur la 3 !",
-        responseNegative:"Vous avez répondu NON à la question 3, c'est pas bien !",
+        responsePositive:"Néanmoins la personne doit avoir explicitement exprimé son consentement de son vivant.",
+        responseNegative:"Selon le principe du consentement présumé, chaque personne est réputée donneur. Toutefois, toute personne est libre de refuser le prélèvement dès lors qu’elle a fait connaître son opposition de son vivant.",
         section:"2"
     },
     "4":{
-        responsePositive:"Bravo, vous êtes d'accord avec nous  sur la 4 !",
-        responseNegative:"Vous avez répondu à la question 4, c'est pas bien !",
+        responsePositive:"La personne peut choisir les organes dont elle est prélevée.",
+        responseNegative:"La personne ne peut choisir les organes dont elle prélevée.",
         section:"2"
-    }
+    },
+     "5":{
+        responsePositive:"Le donneur ne peut connaître l’identité du receveur ni le receveur celle du donneur.",
+        responseNegative:"Le donneur peut connaître l’identité du receveur et le receveur celle du donneur.",
+        section:"2"
+    },
+     "6":{
+        responsePositive:"Le prélèvement d’organes sur une personne vivante qui en fait le don peut être opéré.",
+        responseNegative:"Le prélèvement d’organes sur une personne vivante qui en fait le don ne peut pas être opéré.",
+        section:"3"
+    },
+     "7":{
+        responsePositive:"Une rémunération en contrepartie d’un don d’organe est possible.",
+        responseNegative:"Aucune rémunération n’est possible en contrepartie d’un don d’organe.",
+        section:"3"
+    },
+     "8":{
+        responsePositive:"Le prélèvement peut être autorisé à des fins non médicales.",
+        responseNegative:"Le prélèvement ne peut être effectué que dans un but thérapeutique.",
+        section:"4"
+    },
+     "9":{
+        responsePositive:"Outre les critères d’urgence et de faisabilité, les comportements et mérites des individus peuvent conditionner l’accès à la greffe ou leur place dans la liste d’attente, lors d’une procédure de greffe d’organes.",
+        responseNegative:"Seuls les critères d’urgence et de faisabilité doivent être pris en compte comme condition d’accès à la greffe d’organes. Aucun critère comportemental ne peut être retenu comme condition d’accès à la greffe.",
+        section:"4"
+    },
 };
 
 var sections = {
-    "1" : 'Première section',
-    "2" : "Deuxième section"
+    "1" : " "
+    "2" : "TITRE Ier. Prélèvement post-mortem",
+    "3" : "TITRE II. Prélèvement sur vivants",
+    "4" : "TITRE III. Conditions d'accès à la greffe"
 }
